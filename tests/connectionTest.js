d9d749c9f49a2c2fa003d3d38dcc987c7fdf7375
Anastasiia <anastasiia.sushchenko@edu.umit-tirol.at>
2023-10-15 12:23:35 +0200

    delete testing files

--- tests/connectionTest.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest');
-const server = require('../index');
-const chai = require('chai');
-chai.should();
-const api = supertest.agent(server);
-
-//Todo Unit Tests
-
--- tests/controllerTest.js
+++
@@ -1,4 +0,0 @@
-const expect = require('chai').expect;
-const apiTemperature = require('../public/js/auxiliaryAPI')
-
-//TODO Unit Tests to implement
